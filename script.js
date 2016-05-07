0e5ecb1e1227d455e942090d12201159ba3ff354
Jesse <jessehwang86@yahoo.com>
2016-05-07 14:16:09 -0400

    initial commit

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {
-
+  $(window).scroll(function() {
+    // var x = $(window).scrollTop();
+    console.log("hello")
+  });
   $('#fullpage').fullpage({
         menu: "#menu",
         anchors: ['firstPage', 'secondPage', '3rdPage'],
-        sectionsColor: ['#476369', '#BFCE9E', '#7A8E72'],
+        sectionsColor: ['#476369', 'white', 'white'],
         navigation: true,
         navigationPosition: 'right',
-        navigationTooltips: ['page Numba One', 'Second page', 'Third and last page'],
-        responsiveWidth: 900
+        navigationTooltips: ['Intro', 'Projects', 'About Me'],
+        responsiveWidth: 900,
+        scrollOverflow: true,
+        scrollingSpeed: 1200
       }); //End of FullPage
+
+
+
+// console.log($(window).scrollTop())
+
 
 }); //End of Document.ready
